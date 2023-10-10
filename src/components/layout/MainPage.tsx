48e11e946e8b7fbd5c2580434b4d89c723556d64
Krzysztof Iwanski <k84iwanski@gmail.com>
2023-10-10 22:21:21 +0200

    small update api config

--- src/components/layout/MainPage.tsx
+++ src/components/layout/MainPage.tsx
@@ -19,7 +19,7 @@ export const MainPage = () => {
         <h1>To jest strona główna</h1>
         <p>Znajdziesz tu podgląd dostępnych treningów.</p>
         <p><strong>Timer Training APP</strong><br/>To aplikacja służąca do
-            zaawansowanego odmierzania przerw pomiędzy poszczególnymi ćwiczeniami w treningu, <br/>przede wszystkim chodzi o to, że można ustawić różnej długości przerw dla różnych ćwiczeń.</p>
+            zaawansowanego odmierzania przerw pomiędzy poszczególnymi ćwiczeniami w treningu, <br/>dodając trening można ustawić różnej długości przerwy dla różnych ćwiczeń.</p>
         <p>Możesz też dodać swój własny trening wybierając odpowiednią sekcję z
             menu.</p>
         <h2>WYBIERZ Z MENU U GÓRY CO CHCESZ ZROBIĆ</h2>
