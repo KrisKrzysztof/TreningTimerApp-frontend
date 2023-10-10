04f09af24e742a53cb0f5c033c7f3d655aa075cb
Krzysztof Iwanski <k84iwanski@gmail.com>
2023-10-10 15:24:29 +0200

    updated header visual and functionality

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,20 +1,16 @@
-import {useNavigate} from "react-router-dom";
-
-export const Header = () => {
-    const navigate = useNavigate();
-    const goToMainPage = () => {
-        navigate('/');
-    };
-    const goToStartTraining = () => {
-        navigate('/training');
-    };
-    const goToEdit = () => {
-        navigate('/edit');
-    };
-
-    return <header className="App-header">
-        <h1 className="navi header-element" onClick={goToMainPage}>Trening Timer APP</h1>
-        <h2 className="navi header-element" onClick={goToStartTraining}>Rozpocznij trening</h2>
-        <h2 className="navi header-element" onClick={goToEdit}>Sekcja edycji treningów</h2>
+export const Header = () => (
+    <header className="App-header">
+        <a
+            className="header-element title"
+            href="/"
+        >Training Timer APP</a>
+        <a
+            className="header-element"
+            href="/training"
+        >ROZPOCZNIJ TRENING</a>
+        <a
+            className="header-element"
+            href="/edit"
+        >SEKCJA EDYCJI TRENINGÓW</a>
     </header>
-}
+)
