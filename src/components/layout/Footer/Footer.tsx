9ed86aa366644b555868d9050d4c5f4abdd6a583
Krzysztof Iwanski <k84iwanski@gmail.com>
2023-11-07 21:37:27 +0100

    small fix on Main Page

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -2,9 +2,6 @@ export const Footer = () => {
     return <div className="page">
         <br/><hr/><hr/>
         <small>
-            <p>
-                To jest wersja rozwojowa aplikacji, wiele funkcjonalności wymaga jeszcze dopracowania.
-            </p>
             <p>© Wszelkie prawa zastrzeżone.</p>
             <p>
                 Kontakt z autorem: <a className="sign" href="https://www.facebook.com/krzysztof.ivan/">facebook</a>.
